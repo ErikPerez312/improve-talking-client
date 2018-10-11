c59f9ac77957d4989f52274e7673d2bedb7b369a
ErikPerez312 <theerikperez@gmail.com>
2018-10-11 13:37:58 -0700

    Refactor - closure scopes, constraint definitions, and method names

--- improve-talking/ViewControllers/InitialViewController.swift
+++ improve-talking/ViewControllers/InitialViewController.swift
@@ -9,17 +9,24 @@
 import UIKit
 
 class InitialViewController: UIViewController {
-
-    //MARK: VARIABLES
     
-    //MARK: OUTLETS
-    var iconImageView: UIImageView = {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        hideKeyboardWhenTappedAround()
+        view.backgroundColor = #colorLiteral(red: 0.934114673, green: 0.9433633331, blue: 0.9433633331, alpha: 1)
+        addSubviews()
+        setConstraints()
+    }
+    
+    // MARK: - Private
+    
+    private var iconImageView: UIImageView = {
         var imageView = UIImageView()
         imageView.image = #imageLiteral(resourceName: "toast")
         return imageView
     }()
     
-    var appNameLabel: UILabel = {
+    private var appNameLabel: UILabel = {
         var label = UILabel()
         label.textColor = #colorLiteral(red: 0.2323930722, green: 0.2516170577, blue: 0.2795447335, alpha: 1)
         label.text = "Toast"
@@ -32,7 +39,8 @@ class InitialViewController: UIViewController {
         return label
     }()
     
-    var usernameTextField: UITextField = {
+    // TODO: Refactor repeated code in textfield closures. Follow DRY principle.
+    private var usernameTextField: UITextField = {
         var textField = UITextField()
         textField.textColor = #colorLiteral(red: 0.231372549, green: 0.2509803922, blue: 0.2745098039, alpha: 1)
         textField.placeholder = "username"
@@ -44,7 +52,8 @@ class InitialViewController: UIViewController {
         return textField
     }()
     
-    var passwordTextField: UITextField = {
+    // TODO: Refactor repeated code in textfield closures. Follow DRY principle.
+    private var passwordTextField: UITextField = {
         var textField = UITextField()
         textField.textColor = #colorLiteral(red: 0.231372549, green: 0.2509803922, blue: 0.2745098039, alpha: 1)
         textField.layer.cornerRadius = 18
@@ -57,24 +66,23 @@ class InitialViewController: UIViewController {
         return textField
     }()
     
-    var continueButton: UIButton = {
+    private var continueButton: UIButton = {
         var button = UIButton()
         button.layer.cornerRadius = 25
         button.backgroundColor = #colorLiteral(red: 1, green: 0.7137254902, blue: 0.03137254902, alpha: 1)
         button.setTitle("Continue", for: .normal)
         button.setTitleColor(UIColor.white, for: .normal)
-//        button.layer.shadowColor = #colorLiteral(red: 0.231372549, green: 0.2509803922, blue: 0.2784313725, alpha: 1)
-//        button.layer.shadowRadius = 2.0
-//        button.layer.shadowOpacity = 1.0
-//        button.layer.shadowOffset = CGSize(width: 0, height: 1)
-//        button.layer.masksToBounds = false
+        //        button.layer.shadowColor = #colorLiteral(red: 0.231372549, green: 0.2509803922, blue: 0.2784313725, alpha: 1)
+        //        button.layer.shadowRadius = 2.0
+        //        button.layer.shadowOpacity = 1.0
+        //        button.layer.shadowOffset = CGSize(width: 0, height: 1)
+        //        button.layer.masksToBounds = false
         button.titleLabel?.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 25)
+        button.addTarget(self, action: #selector(continueButtonPressed(_:)), for: .touchUpInside)
         return button
     }()
     
-    //MARK: FUNCTIONS
-    
-    private func addOutlets() {
+    private func addSubviews() {
         self.view.addSubview(iconImageView)
         self.view.addSubview(appNameLabel)
         self.view.addSubview(usernameTextField)
@@ -83,16 +91,13 @@ class InitialViewController: UIViewController {
     }
     
     private func setConstraints() {
-        
         let iconHeightWidth = UIScreen.main.bounds.height * 0.17
         let iconTopDistance = UIScreen.main.bounds.height * 0.095
         let textFieldWidth = UIScreen.main.bounds.width * 0.616
         let textFieldTopDistance = UIScreen.main.bounds.height * 0.08
         
-        
         iconImageView.snp.makeConstraints { (make) in
-            make.height.equalTo(iconHeightWidth)
-            make.width.equalTo(iconHeightWidth)
+            make.height.width.equalTo(iconHeightWidth)
             make.centerX.equalToSuperview()
             make.top.equalToSuperview().offset(iconTopDistance)
         }
@@ -103,36 +108,26 @@ class InitialViewController: UIViewController {
         }
         
         usernameTextField.snp.makeConstraints { (make) in
-            make.width.equalTo(textFieldWidth)
-            make.height.equalTo(45)
+            make.size.equalTo(CGSize(width: textFieldWidth, height: 45.0))
             make.top.equalTo(appNameLabel.snp.bottom).offset(textFieldTopDistance)
             make.centerX.equalToSuperview()
         }
         
         passwordTextField.snp.makeConstraints { (make) in
-            make.width.equalTo(textFieldWidth)
-            make.height.equalTo(45)
+            make.size.equalTo(CGSize(width: textFieldWidth, height: 45.0))
             make.top.equalTo(usernameTextField.snp.bottom).offset(15)
             make.centerX.equalToSuperview()
         }
         
         continueButton.snp.makeConstraints { (make) in
-            make.width.equalTo(textFieldWidth)
+            make.size.equalTo(CGSize(width: textFieldWidth, height: 65.0))
             make.centerX.equalToSuperview()
-            make.height.equalTo(65)
             make.top.equalTo(passwordTextField.snp.bottom).offset(45)
         }
-        
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        hideKeyboardWhenTappedAround()
-        view.backgroundColor = #colorLiteral(red: 0.934114673, green: 0.9433633331, blue: 0.9433633331, alpha: 1)
-        addOutlets()
-        setConstraints()
+    @objc private func continueButtonPressed(_ sender: UIButton) {
+        // TODO: Implement method body
+        print("\n * InitialViewController->continueButtonPressed(_:): Method body not implemented")
     }
-
-    
-
 }

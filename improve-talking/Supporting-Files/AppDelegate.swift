a8d4d8da4b3925d754f553bb82b8f3a800fa8828
ErikPerez312 <theerikperez@gmail.com>
2018-10-12 13:01:44 -0700

    Add - Navigation controller to view hierarchy

--- improve-talking/Supporting-Files/AppDelegate.swift
+++ improve-talking/Supporting-Files/AppDelegate.swift
@@ -17,9 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let window = UIWindow()
-        let homeViewController = InitialViewController()
+        let initialVewController = InitialViewController()
+        let navigationController = UINavigationController(rootViewController: initialVewController)
+        navigationController.setNavigationBarHidden(true, animated: false)
         self.window = window
-        window.rootViewController = homeViewController
+        window.rootViewController = navigationController
         window.makeKeyAndVisible()
         return true
     }

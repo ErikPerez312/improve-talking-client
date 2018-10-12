861cdde3b1c1b13da0e88e6f99c920bd4370092a
Sunny Ouyang <ShenOuyang.work@gmail.com>
2018-10-11 23:18:38 -0700

    Added UI for chat VC

--- improve-talking/AppDelegate.swift
+++ improve-talking/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let window = UIWindow()
-        let homeViewController = InitialViewController()
+        let homeViewController = ChatViewController(roomName: "Sunny", token: "123")
         self.window = window
         window.rootViewController = homeViewController
         window.makeKeyAndVisible()
--- improve-talking/ViewControllers/ChatViewController.swift
+++ improve-talking/ViewControllers/ChatViewController.swift
@@ -21,11 +21,77 @@ class ChatViewController: UIViewController, TwilioHandlerDelegate {
         fatalError("Method not supported")
     }
     
+    //MARK: OUTLETS
+    var currentTopicLabel: UILabel = {
+        var label = UILabel()
+        label.textColor = UIColor.white
+        label.text = "Current Topic:"
+        label.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 35)
+        return label
+    }()
+    
+    var topicLabel: UILabel = {
+        var label = UILabel()
+        label.textColor = UIColor.white
+        label.text = "Bubble"
+        label.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 65)
+        return label
+    }()
+    
+    var iconImageView: UIImageView = {
+        var imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "Audio")
+        return imageView
+    }()
+    
+    var cancelButton: UIButton = {
+        var button = UIButton()
+        button.setImage(#imageLiteral(resourceName: "Cancel"), for: .normal)
+        button.layer.cornerRadius = 100
+//        button.addTarget(self, action: #selector(chatButtonPressed(_:)), for: .touchUpInside)
+        button.adjustsImageWhenHighlighted = false
+        return button
+    }()
+    
+    //MARK: Private functions
+    private func addOutlets() {
+         [currentTopicLabel, topicLabel, iconImageView, cancelButton].forEach { (subview) in
+            view.addSubview(subview)
+         }
+    }
+    
+    private func setConstraints() {
+        currentTopicLabel.snp.makeConstraints { (maker) in
+            maker.top.equalToSuperview().offset(65)
+            maker.centerX.equalToSuperview()
+        }
+        
+        topicLabel.snp.makeConstraints { (maker) in
+            maker.centerX.equalToSuperview()
+            maker.top.equalTo(currentTopicLabel.snp.bottom).offset(30)
+        }
+        
+        iconImageView.snp.makeConstraints { (maker) in
+            maker.height.width.equalTo(200)
+            maker.top.equalTo(topicLabel.snp.bottom).offset(45)
+            maker.centerX.equalToSuperview()
+        }
+        
+        cancelButton.snp.makeConstraints { (maker) in
+            maker.width.height.equalTo(105)
+            maker.centerX.equalToSuperview()
+            maker.top.equalTo(iconImageView.snp.bottom).offset(75)
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = #colorLiteral(red: 1, green: 0.7137254902, blue: 0.03137254902, alpha: 1)
-        twilioHandler.delegate = self
-        twilioHandler.connectToRoom(withName: roomName, token: token)
+        addOutlets()
+        setConstraints()
+//        twilioHandler.delegate = self
+//        twilioHandler.connectToRoom(withName: roomName, token: token)
     }
     
     // MARK: TwilioHandlerDelegate

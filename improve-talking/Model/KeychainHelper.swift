28371b6c145e2bc3904f2e5ca01ad11d6a453f76
ErikPerez312 <theerikperez@gmail.com>
2018-10-12 13:00:11 -0700

    Add - KeychainSwift pod and custom wrapper

---
+++ improve-talking/Model/KeychainHelper.swift
@@ -0,0 +1,36 @@
+//
+//  KeychainHelper.swift
+//  improve-talking
+//
+//  Created by Erik Perez on 10/11/18.
+//  Copyright © 2018 Erik Perez. All rights reserved.
+//
+
+import Foundation
+import KeychainSwift
+
+/// Type of token that will be stored in Keychain. Raw value is used as Key.
+enum KeychainProperty: String {
+    case authToken
+    case twilioToken
+    case userID
+}
+
+class KeychainHelper {
+    
+    static func save(value: String, as property: KeychainProperty) {
+        shared.set(value, forKey: property.rawValue, withAccess: .accessibleAlwaysThisDeviceOnly)
+    }
+    
+    static func fetch(_ property: KeychainProperty) -> String?{
+        return shared.get(property.rawValue)
+    }
+    
+    static func remove(_ property: KeychainProperty) {
+        shared.delete(property.rawValue)
+    }
+    
+    // MARK: - Private
+    
+    private static let shared = KeychainSwift()
+}

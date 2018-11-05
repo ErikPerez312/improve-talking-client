17e8edd7941b1c7b1bf5ef75ab3729384ea96578
ErikPerez312 <theerikperez@gmail.com>
2018-11-05 14:37:26 -0800

    commit for merge

--- improve-talking/ViewControllers/HomeViewController.swift
+++ improve-talking/ViewControllers/HomeViewController.swift
@@ -23,15 +23,19 @@ class HomeViewController: UIViewController, ToastSocketHandlerDelegate {
     // MARK: ToastSocketHandlerDelegate
     
     func didCreateChatRoom(withName roomName: String, token: String) {
+        showChatButtonActivityIndicator(false)
         let chatViewController = ChatViewController(roomName: roomName, token: token)
         self.present(chatViewController, animated: true, completion: nil)
     }
     
     // MARK: - Private
     
-    private let button = UIButton()
     private let containerView = UIView()
+    private let chatButton = UIButton()
+    
+    private var chatButtonActicityIndicator: UIActivityIndicatorView?
     private var socketHandler: ToastSocketHandler?
+    
     
     private func buildTodayTopic() {
         let containerHeight = UIScreen.main.bounds.height * 0.4755
@@ -133,65 +137,98 @@ class HomeViewController: UIViewController, ToastSocketHandlerDelegate {
         }
     }
     
-    @objc private func HoldDown(sender:UIButton){
+    private func buildChatButton() {
+        chatButton.setImage(#imageLiteral(resourceName: "Phone"), for: .normal)
+        chatButton.imageEdgeInsets = UIEdgeInsetsMake(30, 30, 30, 30)
+        chatButton.layer.cornerRadius = 62.5
+        chatButton.backgroundColor = #colorLiteral(red: 1, green: 0.7137254902, blue: 0.03137254902, alpha: 1)
+        chatButton.addTarget(self, action: #selector(chatButtonPressed(_:)), for: .touchUpInside)
+        chatButton.adjustsImageWhenHighlighted = false
+        
+        chatButton.addTarget(self, action: #selector(holdReleaseInside(sender:)), for: .touchUpInside)
+        chatButton.addTarget(self, action: #selector(holdReleaseOutside(sender:)), for: .touchUpOutside)
+        chatButton.addTarget(self, action: #selector(holdDown), for: .touchDown)
+        chatButton.addTarget(self, action: #selector(animateOnTap), for: .touchUpInside)
+        
+        view.addSubview(chatButton)
+        chatButton.snp.makeConstraints { maker in
+            maker.centerX.equalToSuperview()
+            maker.height.width.equalTo(125)
+            maker.top.equalTo(containerView.snp.bottom).offset(50)
+        }
+        
+        let indicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        self.chatButtonActicityIndicator = indicator
+        indicator.activityIndicatorViewStyle = .white
+        indicator.isHidden = true
+        
+        chatButton.addSubview(indicator)
+        indicator.snp.makeConstraints { maker in
+            maker.center.equalToSuperview()
+        }
+    }
+    
+    private func showChatButtonActivityIndicator(_ flag: Bool) {
+        if flag {
+            chatButton.imageView?.isHidden = true
+            chatButton.isEnabled = false
+            chatButtonActicityIndicator?.isHidden = false
+            chatButtonActicityIndicator?.startAnimating()
+        } else {
+            chatButton.imageView?.isHidden = false
+            chatButton.isEnabled = true
+            chatButtonActicityIndicator?.isHidden = true
+            chatButtonActicityIndicator?.startAnimating()
+        }
+    }
+    
+    private func buildSocketHandler() {
+        // erik c5b5d1647269e44f6d06e3c8a18a8305
+        // andrew 1be416e43410ab84746d6b7218a4481b
+        let usertoken = "1be416e43410ab84746d6b7218a4481b" // TODO: Fetch user token from keychain
+        let handler = ToastSocketHandler(socketURL: ToastAPI.webSocketURL,
+                                         socketOrigin: ToastAPI.webSocketOrigin,
+                                         userToken: usertoken)
+        handler.delegate = self
+        self.socketHandler = handler
+    }
+    
+    @objc private func chatButtonPressed(_ sender: UIButton) {
+        socketHandler?.chatWithRemoteUser()
+        showChatButtonActivityIndicator(true)
+    }
+    
+    @objc private func holdDown(sender:UIButton){
         UIView.animate(withDuration: 0.3,
                        animations: {
-                        self.button.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+                        self.chatButton.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
         })
     }
     
     @objc private func holdReleaseInside(sender:UIButton){
-        button.backgroundColor = #colorLiteral(red: 1, green: 0.7137254902, blue: 0.03137254902, alpha: 1)
+        chatButton.backgroundColor = #colorLiteral(red: 1, green: 0.7137254902, blue: 0.03137254902, alpha: 1)
         UIView.animate(withDuration: 0.2) {
-            self.button.transform = CGAffineTransform.identity
+            self.chatButton.transform = CGAffineTransform.identity
         }
         AudioServicesPlayAlertSound(1519)
     }
     
     @objc private func holdReleaseOutside(sender:UIButton){
-        button.backgroundColor = #colorLiteral(red: 1, green: 0.7137254902, blue: 0.03137254902, alpha: 1)
+        chatButton.backgroundColor = #colorLiteral(red: 1, green: 0.7137254902, blue: 0.03137254902, alpha: 1)
         UIView.animate(withDuration: 0.2) {
-            self.button.transform = CGAffineTransform.identity
+            self.chatButton.transform = CGAffineTransform.identity
         }
     }
     
     @objc private func animateOnTap() {
         UIView.animate(withDuration: 0.4,
                        animations: {
-                        self.button.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+                        self.chatButton.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
         },
                        completion: { _ in
                         UIView.animate(withDuration: 0.3) {
-                            self.button.transform = CGAffineTransform.identity
+                            self.chatButton.transform = CGAffineTransform.identity
                         }
         })
-    }
-    
-    private func buildChatButton() {
-        button.setImage(#imageLiteral(resourceName: "Phone"), for: .normal)
-        button.imageEdgeInsets = UIEdgeInsetsMake(30, 30, 30, 30)
-        button.layer.cornerRadius = 62.5
-        button.backgroundColor = #colorLiteral(red: 1, green: 0.7137254902, blue: 0.03137254902, alpha: 1)
-        button.addTarget(self, action: #selector(chatButtonPressed(_:)), for: .touchUpInside)
-        button.adjustsImageWhenHighlighted = false
-        view.addSubview(button)
-        button.snp.makeConstraints { maker in
-            maker.centerX.equalToSuperview()
-            maker.height.width.equalTo(125)
-            maker.top.equalTo(containerView.snp.bottom).offset(50)
-        }
-    }
-    
-    @objc private func chatButtonPressed(_ sender: UIButton) {
-        socketHandler?.chatWithRemoteUser()
-    }
-    
-    private func buildSocketHandler() {
-        let usertoken = "" // TODO: Fetch user token from keychain
-        let handler = ToastSocketHandler(socketURL: ToastAPI.webSocketURL,
-                                         socketOrigin: ToastAPI.webSocketOrigin,
-                                         userToken: usertoken)
-        handler.delegate = self
-        self.socketHandler = handler
     }
 }
